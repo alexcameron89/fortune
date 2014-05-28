55bf4a816e1e18095d0f6dd5969491bf151d7744
alexcameron89 <alexcameron98@gmail.com>
2014-05-28 11:33:28 -0500

    Adds cases for input, cleans code, and adds fortunes

--- cookie.rb
+++ cookie.rb
@@ -3,16 +3,20 @@ directory = File.expand_path(File.dirname(__FILE__))
 fortunes = File.open("#{directory}/fortunes.txt").read
 @fortune_array = []
 
-fortunes.each_line do |line|
-  @fortune_array.push(line)
-end
+fortunes.each_line { |line| @fortune_array.push(line) }
 
 def cookie
+
 	puts "Would you like to open your fortune cookie? [Yes/No] "
 	input = gets.chomp
 	input.downcase!
-	puts "Your fortune says: #{@fortune_array[rand(@fortune_array.length - 1)]}" if input == "yes"
-	# puts "Your fortune says: '#{@fortunes[rand(42)]}'" if input == "yes"
+
+	case input
+	when "yes" then puts "Your fortune says: #{@fortune_array[rand(@fortune_array.length - 1)]}"
+	when "no" then puts "Too bad, here's your damn fortune: You're ungrateful."
+	else puts "Not sure what you mean. Had you said 'Yes', your fortune would be: #{@fortune_array[rand(@fortune_array.length - 1)]}"
+	end
+
 end
 
 cookie
